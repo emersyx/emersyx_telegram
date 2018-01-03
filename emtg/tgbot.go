a1f0ee8357f827b80453b859feb50748ba27fdc3
Alexandru Geana <alex@alegen.net>
2018-01-03 13:42:07 +0100

    implement telegram events channel
    add the polling for events in telegram, conversion to EUpdate objects and pushing them down the events channel

--- emtg/tgbot.go
+++ emtg/tgbot.go
@@ -1,9 +1,18 @@
 package main
 
 import(
+    "encoding/json"
+    "errors"
+    "time"
     "emersyx.net/emersyx_apis/emcomapi"
     "emersyx.net/emersyx_apis/emtgapi"
+    "emersyx.net/emersyx_telegram/tgbotapi"
 )
+
+type apiResponse struct {
+    OK      bool            `json:"ok"`
+    Result  json.RawMessage `json:"result"`
+}
 
 type TelegramBot struct {
     identifier string
@@ -13,7 +22,58 @@ type TelegramBot struct {
     updates chan emcomapi.Event
 }
 
-func (bot TelegramBot) startPolling() {
+func (bot TelegramBot) startPollingUpdates() {
+    go func() {
+        var offset int64
+        for {
+            updates, err := bot.getUpdates(offset)
+            if err != nil {
+                // if an error occurs, wait for 5 seconds
+                time.Sleep(5)
+                continue
+            }
+            for _, update := range updates {
+                eupdate := emtgapi.EUpdate {
+                    Update : update,
+                    Source : bot.identifier,
+                }
+                bot.updates <- eupdate
+            }
+            // the next offset value is the highest current value plus 1
+            offset = updates[ len(updates) - 1 ].UpdateID + 1
+        }
+    }()
+}
+
+func (bot TelegramBot) getUpdates(offset int64) ([]emtgapi.Update, error) {
+    var apiresp apiResponse
+    var updates []emtgapi.Update
+
+    params := (NewTelegramParameters()).(*TelegramParameters)
+    params.Offset(offset)
+    params.Limit(bot.updatesLimit)
+    params.Timeout(bot.updatesTimeout)
+
+    resp, err := tgbotapi.GetUpdates(params.values)
+    if err != nil {
+        return updates, err
+    }
+
+    err = json.Unmarshal( []byte(resp), &apiresp )
+    if err != nil {
+        return updates, err
+    }
+
+    if apiresp.OK == false {
+        return updates, errors.New("The ok field in the Bot API response is false.")
+    }
+
+    err = json.Unmarshal(apiresp.Result, &updates)
+    if err != nil {
+        return updates, err
+    }
+
+    return updates, nil
 }
 
 func NewTelegramBot(options ...func(emtgapi.TelegramBot) error) (emtgapi.TelegramBot, error) {
@@ -35,7 +95,7 @@ func NewTelegramBot(options ...func(emtgapi.TelegramBot) error) (emtgapi.Telegra
     }
 
     // start polling the Telegram servers for updates
-    bot.startPolling()
+    bot.startPollingUpdates()
 
     return bot, nil
 }
--- emtg/tgparams.go
+++ emtg/tgparams.go
@@ -11,6 +11,30 @@ type TelegramParameters struct {
     values url.Values
 }
 
+func (params *TelegramParameters) Offset(value int64) error {
+    params.values.Set("offset", strconv.FormatInt(value, 10))
+    return nil
+}
+
+func (params *TelegramParameters) Limit(value uint) error {
+    params.values.Set("limit", strconv.FormatUint(uint64(value), 10))
+    return nil
+}
+
+func (params *TelegramParameters) Timeout(value uint) error {
+    params.values.Set("timeout", strconv.FormatUint(uint64(value), 10))
+    return nil
+}
+
+func (params *TelegramParameters) AllowedUpdates(values ...string) error {
+    for _, value := range values {
+        if len(value) > 0 {
+            params.values.Add("allowed_updates", value)
+        }
+    }
+    return nil
+}
+
 func (params *TelegramParameters) ChatID(value string) error {
     if len(value) == 0 {
         return errors.New("The chat_id value cannot have zero length.")
@@ -31,7 +55,7 @@ func (params *TelegramParameters) ParseMode(value string) error {
     if len(value) == 0 {
         return errors.New("The parse_mode value cannot have zero length.")
     }
-    params.values.Set("text", value)
+    params.values.Set("parse_mode", value)
     return nil
 }
 
