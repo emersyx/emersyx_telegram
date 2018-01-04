99754cca58d9e5c0e5f2ca47a63fe92d6f731082
Alexandru Geana <alex@alegen.net>
2018-01-04 16:50:39 +0100

    gofmt on tgbotapi/*
    the result of calling gofmt on the files in tgbotapi

--- tgbotapi/tgbotapi_test.go
+++ tgbotapi/tgbotapi_test.go
@@ -1,11 +1,11 @@
 package tgbotapi
 
-import(
-    "flag"
-    "net/url"
-    "os"
-    "strconv"
-    "testing"
+import (
+	"flag"
+	"net/url"
+	"os"
+	"strconv"
+	"testing"
 )
 
 var token = flag.String("apitoken", "", "Telegram BOT API token")
@@ -13,45 +13,44 @@ var recvID = flag.String("recvid", "", "Receiver of test messages")
 var updateOffset = flag.Int64("updoffset", 0, "Value for the offset parameter when calling the getUpdates method.")
 
 func TestMain(m *testing.M) {
-    // get the command line flags
-    flag.Parse()
+	// get the command line flags
+	flag.Parse()
 
-    // initialize the low level Telegram Bot API library
-    Initialize(*token)
+	// initialize the low level Telegram Bot API library
+	Initialize(*token)
 
-    // run the tests
-    os.Exit(m.Run())
+	// run the tests
+	os.Exit(m.Run())
 }
 
 func TestGetUpdates(t *testing.T) {
-    params := url.Values{}
-    params.Add("offset", strconv.FormatInt(*updateOffset, 10))
-    resp, err := GetUpdates(params)
-    if err != nil {
-        t.Fatal(err.Error())
-    }
-    t.Log(resp)
+	params := url.Values{}
+	params.Add("offset", strconv.FormatInt(*updateOffset, 10))
+	resp, err := GetUpdates(params)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Log(resp)
 }
 
 func TestGetMe(t *testing.T) {
-    resp, err := GetMe()
-    if err != nil {
-        t.Fatal(err.Error())
-    }
-    t.Log(resp)
+	resp, err := GetMe()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Log(resp)
 }
 
 func TestSendMessage(t *testing.T) {
-    // in order to obtain the identifier of a user, channel or group, read
-    // https://github.com/GabrielRF/telegram-id#web-channel-id
-    params := url.Values{}
-    params.Add("chat_id", *recvID)
-    params.Add("text", "hello world!")
+	// in order to obtain the identifier of a user, channel or group, read
+	// https://github.com/GabrielRF/telegram-id#web-channel-id
+	params := url.Values{}
+	params.Add("chat_id", *recvID)
+	params.Add("text", "hello world!")
 
-    resp, err := SendMessage(params)
-    if err != nil {
-        t.Fatal(err.Error())
-    }
-    t.Log(resp)
+	resp, err := SendMessage(params)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Log(resp)
 }
-
--- tgbotapi/util.go
+++ tgbotapi/util.go
@@ -1,30 +1,30 @@
 package tgbotapi
 
-import(
-    "bytes"
-    "net/http"
-    "net/url"
+import (
+	"bytes"
+	"net/http"
+	"net/url"
 )
 
 // This function builds the URL for the Telegram Bot API for the API token used in the Initialize function and the
 // specified method.
 func tgurl(apiMethod string) string {
-    return "https://api.telegram.org/bot" + apiToken + "/" + apiMethod
+	return "https://api.telegram.org/bot" + apiToken + "/" + apiMethod
 }
 
 // This function takes an *http.Response argument and returns the body of the response as a string.
 func responseBody(resp *http.Response) string {
-    buf := new(bytes.Buffer)
-    buf.ReadFrom(resp.Body)
-    return buf.String()
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	return buf.String()
 }
 
 // This function checks if all mandatory parameters (specified as argument) are present and not an empty string.
 func checkParams(params url.Values, mandatory ...string) bool {
-    for _, m := range mandatory {
-        if params.Get(m) == "" {
-            return false
-        }
-    }
-    return true
+	for _, m := range mandatory {
+		if params.Get(m) == "" {
+			return false
+		}
+	}
+	return true
 }
